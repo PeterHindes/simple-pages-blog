da24928916ce8544de326eebbc5bf42fa08329ed
Peter Hindes <19994487+PeterHindes@users.noreply.github.com>
2023-12-17 20:19:26 +0100

    build mostly complete

--- src/build.js
+++ src/build.js
@@ -1,5 +1,25 @@
-var showdown  = require('showdown'),
-    converter = new showdown.Converter(),
-    text      = '# hello, markdown!',
-    html      = converter.makeHtml(text);
-    
+const fs = require('fs');
+const path = require('path');
+const showdown  = require('showdown');
+const converter = new showdown.Converter();
+
+function traverseDir(dir, callback) {
+    fs.readdirSync(dir).forEach(file => {
+        let fullPath = path.join(dir, file);
+        if (fs.lstatSync(fullPath).isDirectory()) {
+            traverseDir(fullPath, callback);
+        } else {
+            callback(fullPath);
+        }
+    });
+}
+
+traverseDir('articles', (filePath) => {
+    if (path.extname(filePath) === '.md') {
+        let text = fs.readFileSync(filePath, 'utf8');
+        let html = converter.makeHtml(text);
+        let newFilePath = path.join('public', filePath.slice(0, -3) + '.html');
+        fs.mkdirSync(path.dirname(newFilePath), { recursive: true });
+        fs.writeFileSync(newFilePath, html);
+    }
+});
